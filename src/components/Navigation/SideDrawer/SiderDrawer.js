ce6da6dfdb183e65ac9996de7194eb6fa6be7c51
wasifmiraz <wasifmiraz@mail.com>
2018-09-14 18:06:12 +0600

    resuing the backdrop

--- src/components/Navigation/SideDrawer/SiderDrawer.js
+++ src/components/Navigation/SideDrawer/SiderDrawer.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import classes from './SideDrawer.css';
 import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems'
-import classes from './SideDrawer.css'
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Aux'
 
-const SideDrawer =(props) => (
-<div className={classes.SideDrawer}>
-<div className={classes.Logo}>
-<Logo/>
-</div>
-<nav>
-<NavigationItems/>
-</nav>
-</div>
+
+
+const SideDrawer =(props) => {
+
+
+    return(
+    <Aux>
+    <Backdrop show/>
+        <div>
+            <div className={classes.Logo}>
+           <Logo/>
+        </div>
+    <nav>
+       <NavigationItems/>
+     </nav>
+    </div>
+        </Aux>
 
 );
-
+}
 export default SideDrawer;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -9,7 +9,6 @@ const modal = (props) => (
 <Aux>
 <Backdrop show={props.show} bkdclicked={props.modalClose} />
   <div className={classes.Modal}
-
 style={{
     transform:props.show ? 'translateY(0)' :'translateY(-100vh)',
     opacity:props.show?'1': '0'
