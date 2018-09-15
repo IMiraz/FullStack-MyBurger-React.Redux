e22370962fc09fd48c8b94214b313f074411dbda
wasifmiraz <wasifmiraz@mail.com>
2018-09-15 18:08:59 +0600

    Checkout and checkout summer dummy testing

---
+++ src/components/CheckoutSummary/CheckoutSummery.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Burger from '../../components/Burger/Burger'
+import Buttton from '../UI/Button/Button'
+import classes from './CheckoutSummary.css'
+
+const  checkoutSummary = (props) =>
+(
+    <div className={classes.CheckoutSummary}>
+    <h1>We hope that it testy :) </h1>
+     <div style={{width:'300px', height:'300px', margin:'auto'}}>
+
+<Burger ingredients={props.ingredients}/>
+     </div>
+<Buttton btnType="Danger" clicked> CANCLE
+</Buttton>
+<Buttton btnType="Success" clicked>
+SUCCESS
+</Buttton>
+    </div>
+
+)
+export default checkoutSummary
