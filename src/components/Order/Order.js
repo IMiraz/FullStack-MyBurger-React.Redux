9b9775e798bfd394f8b31b8c1897b3dca683b34b
wasifmiraz <wasifmiraz@mail.com>
2018-09-16 17:39:18 +0600

    display 2digit price checkout 2fixing parse way

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -4,7 +4,7 @@ import classes from './Order.css'
 const order = (props) =>(
     <div className={classes.Order}>
     <p>Ingredients : Salad (1)</p>
-    <p>Price: <strong>{props.price}</strong></p>
+    <p>Price: <strong>{Number.parseFloat(props.price).toFixed(2)}</strong></p>
     </div>
 
 );
