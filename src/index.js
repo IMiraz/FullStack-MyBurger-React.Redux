a137a143034ef30e1779c0479fd40839fcc75341
wasifmiraz <wasifmiraz@mail.com>
2018-09-18 16:55:08 +0600

    index file redux setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 import './index.css';
 import App from './App';
 
 import {BrowserRouter} from 'react-router-dom'
-
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './Store/reducer'
+
+const store=createStore(reducer);
+
+
 
 const app = (
+    <Provider store={store}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-
+ </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
